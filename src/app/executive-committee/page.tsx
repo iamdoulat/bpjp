fece21072455825081632680b18a264f943b96ba
Smart Ideas <smartideasbd24@gmail.com>
2025-06-30 09:59:38 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <Card className="s

--- src/app/executive-committee/page.tsx
+++ src/app/executive-committee/page.tsx
@@ -145,18 +145,6 @@ export default function ExecutiveCommitteePage() {
             </CardContent>
         </Card>
 
-        {contentData?.membersContent && (
-             <Card className="shadow-md mt-8">
-                <CardHeader>
-                    <CardTitle>কার্যনির্বাহী কমিটি</CardTitle>
-                     <CardDescription>ভূজপুর প্রবাসী যুব কল্যাণ পরিষদের ২০২৫-২৭ সেশনে কার্যনির্বাহী কমিটি</CardDescription>
-                </CardHeader>
-                <CardContent className="prose dark:prose-invert max-w-none text-sm">
-                   <div dangerouslySetInnerHTML={{ __html: contentData.membersContent.replace(/\n/g, '<br />') }} />
-                </CardContent>
-            </Card>
-        )}
-
         <Card className="shadow-lg">
             <CardHeader>
                 <CardTitle>কার্যনির্বাহী কমিটি সদস্য</CardTitle>
