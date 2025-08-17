110f393759219f7d366e69b4d0fdc121bf679123
Smart Ideas <smartideasbd24@gmail.com>
2025-08-17 10:18:16 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <AboutSection>
    
    wi

--- src/components/landing/AboutSection.tsx
+++ src/components/landing/AboutSection.tsx
@@ -28,7 +28,7 @@ export function AboutSection() {
   }, []);
 
   return (
-    <section id="about" className="pt-5 pb-16 md:py-24 bg-background">
+    <section id="about" className="py-3 bg-background">
       <div className="container">
         <div className="grid items-center gap-12 lg:grid-cols-2">
           <div className="space-y-4">
