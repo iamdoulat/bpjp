38877dc37a1143fb42e3bcfc11643697f4c200c4
Smart Ideas <smartideasbd24@gmail.com>
2025-08-22 18:31:32 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <CardContent class

--- src/components/landing/AdvisoryBoardSection.tsx
+++ src/components/landing/AdvisoryBoardSection.tsx
@@ -48,7 +48,7 @@ export function AdvisoryBoardSection() {
         <div className="mt-12">
           {loading ? (
             <div className="grid grid-cols-1 gap-8 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
-              {[...Array(5)].map((_, i) => <MemberCardSkeleton key={i} />)}
+              {[...Array(4)].map((_, i) => <MemberCardSkeleton key={i} />)}
             </div>
           ) : error ? (
             <Alert variant="destructive">
@@ -57,10 +57,10 @@ export function AdvisoryBoardSection() {
             </Alert>
           ) : members.length > 0 ? (
             <div className="w-[95%] mx-auto">
-              <Carousel opts={{ align: "start", loop: members.length > 5 }} className="w-full max-w-xs sm:max-w-md md:max-w-lg lg:max-w-2xl xl:max-w-5xl mx-auto">
+              <Carousel opts={{ align: "start", loop: members.length > 4 }} className="w-full max-w-xs sm:max-w-md md:max-w-lg lg:max-w-2xl xl:max-w-5xl mx-auto">
                 <CarouselContent className="-ml-4">
                   {members.map((member) => (
-                    <CarouselItem key={member.id} className="pl-4 basis-full sm:basis-1/2 lg:basis-1/3 xl:basis-1/5">
+                    <CarouselItem key={member.id} className="pl-4 basis-full sm:basis-1/2 lg:basis-1/3 xl:basis-1/4">
                       <div className="p-1 h-full">
                         <MemberCard member={member} />
                       </div>
