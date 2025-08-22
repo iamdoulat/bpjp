6fc94b66b22fafdbd984a7dafe82e2ad85cfd646
Smart Ideas <smartideasbd24@gmail.com>
2025-08-22 18:19:56 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <AboutSection>
    
    wi

--- src/components/landing/AboutSection.tsx
+++ src/components/landing/AboutSection.tsx
@@ -28,7 +28,7 @@ export function AboutSection() {
   }, []);
 
   return (
-    <section id="about" className="py-5 bg-background pr-[5px]">
+    <section id="about" className="py-5 bg-background px-[25px]">
       <div className="container">
         <div className="grid items-start gap-12 lg:grid-cols-2">
           <div className="space-y-4 pl-[10px]">
