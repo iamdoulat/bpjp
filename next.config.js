77a6a7739a2ffe9bad13ed7e31cad3e86e6635e5
Smart Ideas <smartideasbd24@gmail.com>
2025-08-22 16:03:17 +0000

    The NextJS app server is having trouble starting. Please identify what c

---
+++ next.config.js
@@ -0,0 +1,40 @@
+
+const WithPWAInit = require('@ducanh2912/next-pwa').default;
+
+const withPWA = WithPWAInit({
+  dest: 'public',
+  disable: process.env.NODE_ENV === 'development',
+  register: true,
+  skipWaiting: true,
+  cacheOnFrontEndNav: true,
+  reloadOnOnline: true,
+});
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  /* config options here */
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'firebasestorage.googleapis.com',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
+};
+
+module.exports = withPWA(nextConfig);
