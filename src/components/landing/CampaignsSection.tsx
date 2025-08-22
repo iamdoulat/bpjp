5da0b524d6a8ed198909332ee2c8a3393802ae4b
Smart Ideas <smartideasbd24@gmail.com>
2025-08-22 18:18:01 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <Carousel opts={"a

--- src/components/landing/CampaignsSection.tsx
+++ src/components/landing/CampaignsSection.tsx
@@ -79,7 +79,7 @@ export function CampaignsSection() {
                 align: "center",
                 loop: activeCampaigns.length > 3,
               }}
-              className="w-full px-[35px] my-[10px] mx-[20px]"
+              className="w-full px-[35px] my-[5px] mx-[20px]"
             >
               <CarouselContent className="-ml-4">
                 {activeCampaigns.map((campaign) => (
