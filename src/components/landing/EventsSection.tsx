b42524e739688dbd7a07678c4715b11fda14968b
Smart Ideas <smartideasbd24@gmail.com>
2025-08-22 18:05:49 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <Carousel opts={"a

--- src/components/landing/EventsSection.tsx
+++ src/components/landing/EventsSection.tsx
@@ -121,7 +121,7 @@ export function EventsSection() {
                 align: "center",
                 loop: upcomingEvents.length > 3,
               }}
-              className="w-full px-[25px]"
+              className="w-[95%] px-[25px]"
             >
               <CarouselContent className="-ml-4">
                 {upcomingEvents.map((event) => (
