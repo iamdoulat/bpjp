5729f05119909dddcb5e6193a3806f89c6f1564e
Smart Ideas <smartideasbd24@gmail.com>
2025-08-26 07:22:11 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <Footer>
    
    with the

--- src/components/landing/Footer.tsx
+++ src/components/landing/Footer.tsx
@@ -124,8 +124,21 @@ export function Footer() {
             </div>
           </div>
 
-          {/* Spacer */}
-          <div></div>
+          {/* Facebook Widget */}
+          <div className="flex justify-center">
+            <iframe
+              src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2Fbpjkp&tabs=timeline&width=300&height=350&small_header=true&adapt_container_width=true&hide_cover=false&show_facepile=true&appId"
+              width="300"
+              height="350"
+              style={{ border: 'none', overflow: 'hidden' }}
+              scrolling="no"
+              frameBorder="0"
+              allowFullScreen={true}
+              allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share"
+              title="BPJP Facebook Page"
+              className="rounded-lg shadow-md"
+            ></iframe>
+          </div>
 
           {/* Feedback Form */}
           <div className="space-y-4">
