6746b63116fb7c7c5874eb73d48703d467ab83bb
Smart Ideas <smartideasbd24@gmail.com>
2025-08-27 06:17:49 +0000

    Create a Sitemap & Robots.txt
    Use next-sitemap
    .
    
    Install:
    
    npm install

---
+++ next-sitemap.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: process.env.SITE_URL || 'https://bpjp.vercel.app',
+  generateRobotsTxt: true, // (Optional)
+  // ...other options
+}
