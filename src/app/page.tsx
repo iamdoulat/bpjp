8dd54f7ad0cf16134add50de92d3cd18def8116a
Smart Ideas <smartideasbd24@gmail.com>
2025-08-27 06:06:24 +0000

    Add in Landing page import { NextSeo } from "next-seo"; and
    <NextSeo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,36 +11,50 @@ import { CommitteeSection } from "@/components/landing/CommitteeSection";
 import { Footer } from "@/components/landing/Footer";
 import { AdvisoryBoardSection } from "@/components/landing/AdvisoryBoardSection";
 import { NewsTicker } from "@/components/landing/NewsTicker";
+import { NextSeo } from "next-seo";
 
 export default function LandingPage() {
   return (
-    <div className="flex flex-col min-h-screen bg-[#EEEEEE] dark:bg-background">
-      <LandingNavbar />
-      <NewsTicker />
-      <main className="flex-1">
-        <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-          <HeroSection />
-          <div className="mt-5">
-            <CampaignsSection />
+    <>
+      <NextSeo
+        title="ভূজপুর প্রবাসী যুব কল্যাণ পরিষদ"
+        description="২০১৮ সালে প্রতিষ্ঠিত, এটি একটি অলাভজনক প্রতিষ্ঠান (নিবন্ধন নং: ২০১৮/এ)। আমাদের প্রধান কার্যালয় অবস্থিত — কাজির হাট, থানা: ভূজপুর, উপজেলা: ফটিকছড়ি, জেলা: চট্টগ্রাম-৪৩৫০, বাংলাদেশ।"
+        openGraph={{
+          url: "bpjp.vercel.app",
+          title: "ভূজপুর প্রবাসী যুব কল্যাণ পরিষদ",
+          description: "২০১৮ সালে প্রতিষ্ঠিত, এটি একটি অলাভজনক প্রতিষ্ঠান (নিবন্ধন নং: ২০১৮/এ)। আমাদের প্রধান কার্যালয় অবস্থিত — কাজির হাট, থানা: ভূজপুর, উপজেলা: ফটিকছড়ি, জেলা: চট্টগ্রাম-৪৩৫০, বাংলাদেশ।",
+          images: [{ url: "/_next/image?url=https%3A%2F%2Ffirebasestorage.googleapis.com%2Fv0%2Fb%2Fimpactboard-opo8r.firebasestorage.app%2Fo%2Flogo%252Fbpjp-logo.jpg%3Falt%3Dmedia%26token%3De2e5e262-9340-4e34-89da-1d486a5b9399&w=32&q=75", alt: "My Site" }],
+          site_name: "ভূজপুর প্রবাসী যুব কল্যাণ পরিষদ",
+        }}
+      />
+      <div className="flex flex-col min-h-screen bg-[#EEEEEE] dark:bg-background">
+        <LandingNavbar />
+        <NewsTicker />
+        <main className="flex-1">
+          <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+            <HeroSection />
+            <div className="mt-5">
+              <CampaignsSection />
+            </div>
+            <div className="mt-5">
+              <EventsSection />
+            </div>
+            <div className="mt-5">
+              <MissionSection />
+            </div>
+            <div className="mt-5">
+              <AboutSection />
+            </div>
+            <div className="mt-5">
+              <CommitteeSection />
+            </div>
+            <div className="mt-5 mb-5">
+              <AdvisoryBoardSection />
+            </div>
           </div>
-          <div className="mt-5">
-            <EventsSection />
-          </div>
-          <div className="mt-5">
-            <MissionSection />
-          </div>
-          <div className="mt-5">
-            <AboutSection />
-          </div>
-          <div className="mt-5">
-            <CommitteeSection />
-          </div>
-          <div className="mt-5 mb-5">
-            <AdvisoryBoardSection />
-          </div>
-        </div>
-      </main>
-      <Footer />
-    </div>
+        </main>
+        <Footer />
+      </div>
+    </>
   );
 }
