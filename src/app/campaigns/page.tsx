a0f7e4fae55a80b13eea960c43fd8706c5936a78
Smart Ideas <smartideasbd24@gmail.com>
2025-08-28 08:47:33 +0000

    Also apply style, size and functionality on '/campaigns' page Active Cam

--- src/app/campaigns/page.tsx
+++ src/app/campaigns/page.tsx
@@ -99,7 +99,7 @@ export default function BrowseCampaignsPage() {
           </Alert>
         ) : campaignsToShow.length > 0 ? (
           <>
-            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-x-6 gap-y-8">
+            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
               {campaignsToShow.map(campaign => (
                 <CampaignCard key={campaign.id} campaign={campaign} isPublicView={true} />
               ))}
